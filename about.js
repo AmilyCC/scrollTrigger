cac3a1062b8bfa95d9bb79a99e0157c27e6beb66
AmilyCC <aymaiclhyu777@gmail.com>
2021-08-20 10:07:09 +0800

    init commit

---
+++ about.js
@@ -0,0 +1,41 @@
+function inputingFb(){
+    var img=document.getElementById("imageFb");
+    img.src="img/fb2.png";    
+    }
+function leavingFb(){
+    var img=document.getElementById("imageFb");
+    img.src="img/fb.png";    
+    }
+
+function inputingIns(){
+    var img=document.getElementById("imageIns");
+    img.src="img/ins2.png";    
+    }
+function leavingIns(){
+    var img=document.getElementById("imageIns");
+    img.src="img/ins.png";    
+    }
+
+function inputingFbFt(){
+    var img=document.getElementById("imageFbFt");
+    img.src="img/fb2.png";    
+    }
+function leavingFbFt(){
+    var img=document.getElementById("imageFbFt");
+    img.src="img/fb.png";    
+    }
+
+function inputingInsFt(){
+    var img=document.getElementById("imageInsFt");
+    img.src="img/ins2.png";    
+    }
+function leavingInsFt(){
+    var img=document.getElementById("imageInsFt");
+    img.src="img/ins.png";    
+    }
+$(document).ready(function() {
+    $('.AboutTilte h1').slideToggle(function(event) {
+        $('.AboutTilte h1').stop().slideToggle(0500);
+    });
+});
+
